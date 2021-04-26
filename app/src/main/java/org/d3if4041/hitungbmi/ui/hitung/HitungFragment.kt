6c44b07f83c3a33a5aeda2dde1da6186dd14d8ca
billyrefanto <mochbillyrefanto@gmail.com>
2021-04-27 00:25:13 +0700

    3.2 Memperbaiki navigasi dengan view mode

--- app/src/main/java/org/d3if4041/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4041/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if4041.hitungbmi.ui
+package org.d3if4041.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -15,10 +15,10 @@ import org.d3if4041.hitungbmi.databinding.FragmentHitungBinding
 
 
 class HitungFragment : Fragment() {
-    
+
     private val viewModel: HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,9 +28,8 @@ class HitungFragment : Fragment() {
             layoutInflater, container, false
         )
         binding.button.setOnClickListener { hitungBmi() }
-        binding.saranButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(
-                HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))
+        binding.saranButton.setOnClickListener {
+            viewModel.mulaiNavigasi()
         }
         binding.resetButton.setOnClickListener { resetBMI() }
         binding.shareButton.setOnClickListener { shareData() }
@@ -41,6 +40,12 @@ class HitungFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.getNavigasi().observe(viewLifecycleOwner,{
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
+        })
+
         viewModel.getHasilBmi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
             binding.bmiTextView.text = getString(R.string.bmi_x, it.bmi)
@@ -48,6 +53,7 @@ class HitungFragment : Fragment() {
                 getKategori(it.kategori))
             binding.buttonGroup.visibility = View.VISIBLE
         })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
